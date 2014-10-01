32df407614ead47031b52ad0bac27740607fb536
Salma-El-Alaoui <s.elalaouitalibi@gmail.com>
2014-10-01 10:13:43 +0200

    spécification de la matrice de contraintes

---
+++ Probleme.m
@@ -0,0 +1,25 @@
+%% Projet d'aide à la décision 
+% 
+%
+%  
+%  ------------
+% 
+%% Définition des contraintes générales
+
+% vecteur de la quantité de produits par semaine
+%X =[xa; xb; xc; xd; xe; xf];
+
+% Nous avons défini des contraintes suivantes:
+%   - temps d'usage de chaque machine < 16 h par jour
+%   - quantité de matières premières  utilisées inférieures aux seuils
+%   précisés
+%   - les quantités des produits doivent êtres positives
+
+% matrice de contraintes générales
+A = [8 15 0 5 0 10; 7 1 2 15 7 12; 8 1 11 0 10 25; 2 10 5 4 13 7; 5 0 0 7 10 25; 5 5 3 12 8 0; 1 2 1 5 0 2; 2 2 1 2 2 1; 1 0 3 2 2 0];
+% vecteur de seuils
+B = [ 4800 ; 4800; 4800; 4800; 4800; 4800; 350; 620; 485];
+% vecteur de containtes de domaine
+LB = zeros (6,1);
+
+%% ================ Partie 1: Programmation linéaire multi-critères   ================
