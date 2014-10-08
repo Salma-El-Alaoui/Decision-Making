2aa2aef12e419b6de4d0d018c1f98927b827d5ea
ziedt <zied.thabet1@gmail.com>
2014-10-08 11:33:06 +0200

    Electre I - sueillage

---
+++ seuillage_electre.m
@@ -0,0 +1,21 @@
+function [ mat_seuillage ] = seuillage_electre( s,v, mat_jugement )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+mat_seuillage = zeros (size(mat_jugement));
+mat_concordance = concordance(mat_jugement);
+mat_discordance = discordance(mat_jugement);
+
+[L,C] = size(mat_concordance);
+for i=1:L
+    for j=1:C
+        if((mat_concordance(i,j) >= s) && (mat_discordance(i,j)<=v))
+            mat_seuillage(i,j) = 1;
+        end
+    end
+end
+
+
+
+end
+
