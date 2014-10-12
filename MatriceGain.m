ff73f003af15e16b61e1f71f5a2eb4ee5d520198
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2014-10-12 23:38:21 +0200

    estimating the quality of the solution -- generate a radar chart with excel

--- MatriceGain.m
+++ MatriceGain.m
@@ -7,8 +7,8 @@ ecartAtel = abs((Xatel(1) + Xatel(2)+Xatel(3))- (Xatel(4) + Xatel(5)+Xatel(6)));
 ecartStock = abs((Xstock(1) + Xstock(2)+Xstock(3))- (Xstock(4) + Xstock(5)+Xstock(6)));
 ecartCom = abs((Xcom(1) + Xcom(2)+Xcom(3))- (Xcom(4) + Xcom(5)+Xcom(6)));
 ecartPers = abs((Xpersonnel(1) + Xpersonnel(2)+Xpersonnel(3))- (Xpersonnel(4) + Xpersonnel(5)+Xpersonnel(6)));
-ecartM3=abs((Xm3(1) + Xm3(2) + Xm3(3)) - (Xm3(1) + Xm3(2) + Xm3(3)));
-ecartM5=abs((Xm5(1) + Xm5(2) + Xm5(3)) - (Xm5(1) + Xm5(2) + Xm5(3)));
+ecartM3=abs((Xm3(1) + Xm3(2) + Xm3(3)) - (Xm3(4) + Xm3(5) + Xm3(6)));
+ecartM5=abs((Xm5(1) + Xm5(2) + Xm5(3)) - (Xm5(4) + Xm5(5) + Xm5(6)));
 
  MG = [ fcompta'*Xcompta, fatel'*Xcompta, fstock'*Xcompta,ecartCompta, fpersonnel'*Xcompta, fm3'*Xcompta, fm5'*Xcompta;
         fcompta'*Xatel, fatel'*Xatel, fstock'*Xatel, ecartAtel, fpersonnel'*Xatel, fm3'*Xatel, fm5'*Xatel;
---
+++ Satisfaction.m
@@ -0,0 +1,22 @@
+function [VS, PM,Y] = Satisfaction( MG,X,fcompta , fatel, fstock,fpers )
+%vecteur qui représente le pourcentage de staisfaction de l'objectif de
+%chaque responsable.
+%X est la solution choisie
+%MG est la matrice de gain
+
+VS = zeros(5,1);
+%Point de mire
+PM = zeros(5,1);
+for i=1:5
+    PM(i)= MG(i,i);
+end 
+ecart=abs((X(1) + X(2) + X(3)) - (X(4) + X(5) + X(6)));
+echelle = X(1) + X(2) + X(3) + X(4) + X(5) + X(6);
+Y=[ fcompta'*X; fatel'*X; fstock'*X;ecart;fpers'*X];
+for i=1:3
+VS(i)= max(0,(1-(abs(PM(i)-Y(i))/PM(i))));
+end
+VS(4)=max(0, 1-(ecart/echelle));
+VS(5)= max(0,(1-(abs(Y(5)-PM(5))/PM(5))));
+end
+
