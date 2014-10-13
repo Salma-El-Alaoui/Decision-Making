46d77c5bbd757757e8513ec8078526cbd5856272
ziedt <zied.thabet1@gmail.com>
2014-10-13 22:45:08 +0200

    Corrected BUG on calculating concordance pondérée

--- concordance.m
+++ concordance.m
@@ -1,6 +1,6 @@
-function [ mat_concordance ] = concordance( jugement)
+function [ mat_concordance ] = concordance( jugement, poids)
 %UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+%   poids : vecteur ligne des poids affectés à chaque critère [1,1,1,1] ou [5,2,3,2]
 
 [L,C] = size(jugement);
 mat_concordance = zeros(L);
@@ -9,7 +9,7 @@ for i=1:L
     for k=1:L
         for j=1:C
             if (jugement(i,j) >= jugement(k,j))
-                mat_concordance(i,k) = mat_concordance(i,k) +1;
+                mat_concordance(i,k) = mat_concordance(i,k) + poids(1,j);
             end
         end
     end
